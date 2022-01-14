5e5573d1bbd3d0272d6d881e833035e32acc2dbd
Filippo Ciandy <filippo.ciandy10@gmail.com>
2022-01-13 22:57:48 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React from "react";
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from 'axios'
+import Button from '@mui/material/Button';
+// import pages
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+//import components
+import Navbar from "./components/Navbar";
+
 
 function App() {
+  const [main, setMain] = useState(true) //If user is currently in main page 
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [year, setYear] = useState(2000)
+
+  const getData = async() =>{
+    try {
+      if (main){
+        const url = `https://api.nasa.gov/planetary/apod?api_key=lYOcIYZx1dE9HcZHcx96YYvGmPncGkXQbgTBYUDO&count=15`
+      }
+      else{
+        const url = `https://api.nasa.gov/planetary/apod?api_key=lYOcIYZx1dE9HcZHcx96YYvGmPncGkXQbgTBYUDO&start_date=${year}-01-01&end_date=${year}-01-15&thumbs='True'`
+      }
+      const {example} = await axios.get(url) 
+      setData(example)
+      setLoading(false)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/pages/Favorites.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Favorites() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">Favorites section</h1>
+      <p>
+        this is the favorites section
+      </p>
+    </section>
+  );
+}
