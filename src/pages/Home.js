e615d132d24e625d30a19fd96108ac1ad9ecaea7
Filippo Ciandy <filippo.ciandy10@gmail.com>
2022-01-18 22:37:50 -0700

    Updated home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,42 +12,58 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 const Home = ({ data }) => {
   // console.log(data);
   return (
-    <Card
-      sx={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      {data.map((images) => (
-        <CardMedia>
-          <CardContent>
+    <Grid container>
+      <Grid item xs={6}>
+        {data.map((images) => (
+          <Box>
             <img src={images.url} />
-            <Typography> {images.title} </Typography>
-            <Grid container>
-              <Grid item xs={10}>
-                <Typography> {images.explanation} </Typography>
-              </Grid>
-            </Grid>
-            <Grid item xs={2}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "flex-end",
-                  p: 1,
-                }}
-              >
-                <Typography> {images.date}</Typography>
-                <IconButton aria-label="like">
-                  <FavoriteIcon />
-                </IconButton>
-              </Box>
-            </Grid>
-          </CardContent>
-        </CardMedia>
-      ))}
-    </Card>
+            <Typography>{images.explanation}</Typography>
+            <Typography>{images.data}</Typography>
+            <Typography> {images.date}</Typography>
+            <IconButton>
+              {" "}
+              <FavoriteIcon />{" "}
+            </IconButton>
+          </Box>
+        ))}
+      </Grid>
+    </Grid>
+    // <Card
+    //   sx={{
+    //     width: "100%",
+    //     display: "flex",
+    //     flexDirection: "column",
+    //   }}
+    // >
+    //   {data.map((images) => (
+
+    //   ))}
+    // </Card>
   );
 };
-
+//  <CardMedia>
+//     <CardContent>
+//       <img src={images.url} />
+//       <Typography> {images.title} </Typography>
+//       <Grid container>
+//         <Grid item xs={10}>
+//           <Typography> {images.explanation} </Typography>
+//         </Grid>
+//       </Grid>
+//       <Grid item xs={2}>
+//         <Box
+//           sx={{
+//             display: "flex",
+//             justifyContent: "flex-end",
+//             p: 1,
+//           }}
+//         >
+//           <Typography> {images.date}</Typography>
+//           <IconButton aria-label="like">
+//             <FavoriteIcon />
+//           </IconButton>
+//         </Box>
+//       </Grid>
+//     </CardContent>
+//   </CardMedia>
 export default Home;
